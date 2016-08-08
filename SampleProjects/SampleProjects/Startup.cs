46b4d6d94c6cde60c03444bbe202d498b5a8aa52
bhagyarajgowda <bhagyarajgowda08@gmail.com>
2016-08-08 17:13:54 +0530

    initial commint

---
+++ SampleProjects/SampleProjects/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleProjects.Startup))]
+namespace SampleProjects
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
